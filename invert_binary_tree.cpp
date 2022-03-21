9e5a582fffd8e8326b659d7d0fae3790b4e8d7f4
ts13-byte <87604053+ts13-byte@users.noreply.github.com>
2022-03-21 20:10:46 +0530

    Create invert_binary_tree.cpp

---
+++ invert_binary_tree.cpp
@@ -0,0 +1,15 @@
+  TreeNode* invertTree(TreeNode* root) {
+        if(root==NULL){
+            return NULL;
+        }
+       if(root->left){
+         invertTree(root->left);
+       }
+        if(root->right){
+        invertTree(root->right);
+        }
+        TreeNode* temp=root->left;
+        root->left=root->right;
+        root->right=temp;
+        return root;
+    }
